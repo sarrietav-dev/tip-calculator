24d39858692c0ec6984414ec1c91873c705a0729
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-24 12:38:01 -0500

    🔧 Curate Tailwind colors to customs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        'space-mono': ['Space Mono', 'monospace'],
-      },
-      colors: {
-        'primary-cyan': 'hsl(172, 67%, 45%)',
-        'dark-cyan': 'hsl(184, 14%, 56%)',
-        'darker-cyan': 'hsl(186, 14%, 43%)',
-        'darkest-cyan': 'hsl(183, 100%, 15%)',
-        'light-cyan': 'hsl(189, 41%, 97%)',
-        'lighter-cyan': 'hsl(185, 41%, 84%)',
-      },
+    fontFamily: {
+      'space-mono': ['Space Mono', 'monospace'],
+    },
+    colors: {
+      gray: colors.gray,
+      white: colors.white,
+      'primary-cyan': 'hsl(172, 67%, 45%)',
+      'dark-cyan': 'hsl(184, 14%, 56%)',
+      'darker-cyan': 'hsl(186, 14%, 43%)',
+      'darkest-cyan': 'hsl(183, 100%, 15%)',
+      'light-cyan': 'hsl(185, 41%, 84%)',
+      'lighter-cyan': 'hsl(189, 41%, 97%)',
     },
   },
   plugins: [require('@tailwindcss/forms')],
