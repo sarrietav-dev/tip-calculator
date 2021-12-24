aa4d3cd125d2fadd1a27aa29836e5f175105b03f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-24 16:48:15 -0500

    ✨ Reset the store state when clicking the reset button

--- src/components/TheResetButton.vue
+++ src/components/TheResetButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     type="reset"
+    @click="store.$reset()"
     class="
       bg-primary-cyan
       active:bg-light-cyan
@@ -17,3 +18,9 @@
     Reset
   </button>
 </template>
+
+<script lang="ts" setup>
+import useStore from '@/store/mainStore';
+
+const store = useStore();
+</script>
