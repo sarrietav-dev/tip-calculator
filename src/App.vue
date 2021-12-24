5c2b655814942dcc015d4d99f2deea7fe5de5c61
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-23 22:38:44 -0500

    🔧 Add Space Mono font

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="flex flex-col place-content-center bg-teal-150 h-full">
+  <div
+    class="
+      font-space-mono
+      flex flex-col
+      place-content-center
+      bg-teal-150
+      h-full
+    "
+  >
     <div class="flex place-content-center my-10">
       <img alt="Splitter Logo" src="./assets/logo.svg" />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'space-mono': ['Space Mono', 'monospace'],
+      },
       colors: {
         teal: {
           150: 'hsl(185, 41%, 84%)',
