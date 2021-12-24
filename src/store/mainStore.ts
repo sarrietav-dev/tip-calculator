434501ae492707d5a3b7775a83ce8d8a8b237731
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-24 15:32:02 -0500

    ✨ Create Pinia Store

---
+++ src/store/mainStore.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+
+const store = defineStore('main', {
+  state: () => {
+    return {
+      bill: 0,
+      tip: 0,
+      people: 0,
+    };
+  },
+  getters: {
+    tipAmount: ({ tip, bill, people }) => (bill * (tip / 100)) / people,
+    totalAmount(): number {
+      return this.bill / this.people + this.tipAmount;
+    },
+  },
+});
+
+export default store;
