7af4e4ff3c4ec756e96952e26ddc8dee8058a397
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-24 13:24:54 -0500

    💄 Add lg breakpoint styles to ThePanel

--- src/components/TheBalancePanel.vue
+++ src/components/TheBalancePanel.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-darkest-cyan p-6 pt-10 rounded-xl flex flex-col space-y-6">
+  <div class="bg-darkest-cyan p-6 pt-10 rounded-xl flex flex-col lg:grow gap-6">
     <BalanceSection label="Tip Amount" :amount="4.27" />
-    <BalanceSection label="Total" :amount="32.79" />
+    <BalanceSection label="Total" :amount="32.79" class="lg:mb-auto" />
     <TheResetButton />
   </div>
 </template>
--- src/components/ThePanel.vue
+++ src/components/ThePanel.vue
@@ -1,13 +1,19 @@
 <template>
-  <main class="grow bg-white rounded-t-3xl p-7">
-    <form action="" class="space-y-10">
-      <NumberInput label="Bill">
-        <img src="@/assets/icon-dollar.svg" alt="Price icon" class="h-full" />
-      </NumberInput>
-      <TheTipSelector />
-      <NumberInput label="Number of People">
-        <img src="@/assets/icon-person.svg" alt="Person icon" class="h-full" />
-      </NumberInput>
+  <main class="grow lg:grow-0 bg-white rounded-t-3xl p-7">
+    <form action="" class="lg:flex lg:gap-14 space-y-10">
+      <div class="space-y-10">
+        <NumberInput label="Bill">
+          <img src="@/assets/icon-dollar.svg" alt="Price icon" class="h-full" />
+        </NumberInput>
+        <TheTipSelector />
+        <NumberInput label="Number of People">
+          <img
+            src="@/assets/icon-person.svg"
+            alt="Person icon"
+            class="h-full"
+          />
+        </NumberInput>
+      </div>
       <TheBalancePanel />
     </form>
   </main>
