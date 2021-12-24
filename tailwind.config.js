fa2bca9fee185ff08685a7f081b248577c47e16f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-24 11:56:25 -0500

    🔧 Add grayish-cyan color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         'space-mono': ['Space Mono', 'monospace'],
       },
       colors: {
+        "grayish-cyan": "hsl(184, 14%, 56%)",
         teal: {
           150: 'hsl(185, 41%, 84%)',
         },
