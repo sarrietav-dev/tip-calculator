58681ffd3d8ce11a8ca5af07d10fd92dc40fcca3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-24 00:27:13 -0500

    ♻️ Create TheTipSelector component

--- src/components/ThePanel.vue
+++ src/components/ThePanel.vue
@@ -4,26 +4,7 @@
       <NumberInput label="Bill">
         <img src="@/assets/icon-dollar.svg" alt="Price icon" class="h-full" />
       </NumberInput>
-      <div class="space-y-4">
-        <label class="text-gray-500 font-bold">Select Tip %</label>
-        <ul class="grid grid-cols-2 gap-4">
-          <li v-for="tipNumber in tipNumbers">
-            <TipSelectionButton :percentage="tipNumber" />
-          </li>
-          <input
-            type="number"
-            placeholder="Custom"
-            class="
-              border-none
-              bg-gray-100
-              rounded
-              font-bold
-              text-2xl text-green-900 text-right
-              focus:ring-2 focus:ring-green-500
-            "
-          />
-        </ul>
-      </div>
+      <TheTipSelector />
       <NumberInput label="Number of People">
         <img src="@/assets/icon-person.svg" alt="Person icon" class="h-full" />
       </NumberInput>
@@ -32,9 +13,6 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
 import NumberInput from './NumberInput.vue';
-import TipSelectionButton from './TipSelectionButton.vue';
-
-const tipNumbers = ref([5, 10, 15, 25, 50]);
+import TheTipSelector from './TheTipSelector.vue';
 </script>
---
+++ src/components/TheTipSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="space-y-4">
+    <label class="text-gray-500 font-bold">Select Tip %</label>
+    <ul class="grid grid-cols-2 gap-4">
+      <li v-for="tipNumber in tipNumbers">
+        <TipSelectionButton :percentage="tipNumber" />
+      </li>
+      <input
+        type="number"
+        placeholder="Custom"
+        class="
+          border-none
+          bg-gray-100
+          rounded
+          font-bold
+          text-2xl text-green-900 text-right
+          focus:ring-2 focus:ring-green-500
+        "
+      />
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import TipSelectionButton from './TipSelectionButton.vue';
+const tipNumbers = ref([5, 10, 15, 25, 50]);
+</script>
