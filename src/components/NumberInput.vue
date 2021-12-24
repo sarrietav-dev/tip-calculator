821dfb69350714befa25b41e81d0d1218f1d31c1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-23 23:42:10 -0500

    ♻️ Create NumberInput component

---
+++ src/components/NumberInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col font-bold text-gray-500 relative space-y-2">
+    <label :for="label">{{ label }}</label>
+    <div class="absolute bottom-3 left-5 h-5">
+      <slot></slot>
+    </div>
+    <input
+      :id="label"
+      type="number"
+      value="0"
+      class="
+        border-none
+        bg-gray-100
+        rounded
+        font-bold
+        text-xl text-green-900 text-right
+        focus:ring-2 focus:ring-green-500
+      "
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useSlots } from 'vue';
+
+const props = defineProps({
+  label: { required: true, type: String },
+});
+</script>
--- src/components/ThePanel.vue
+++ src/components/ThePanel.vue
@@ -1,29 +1,16 @@
 <template>
   <main class="grow bg-white rounded-t-3xl p-7">
-    <form action="">
-      <div class="flex flex-col font-bold text-gray-500 relative">
-        <label for="bill" class="mb-2">Bill</label>
-        <div class="absolute bottom-3 left-5 h-5">
-          <img
-            src="@/assets/icon-dollar.svg"
-            alt="Price symbol"
-            class="h-full"
-          />
-        </div>
-        <input
-          id="bill"
-          type="number"
-          value="0"
-          class="
-            border-none
-            bg-gray-100
-            rounded
-            font-bold
-            text-xl text-green-900 text-right
-            focus:ring-2 focus:ring-green-500
-          "
-        />
-      </div>
+    <form action="" class="space-y-10">
+      <NumberInput label="Bill">
+        <img src="@/assets/icon-dollar.svg" alt="Price icon" class="h-full" />
+      </NumberInput>
+      <NumberInput label="Number of People">
+        <img src="@/assets/icon-person.svg" alt="Person icon" class="h-full" />
+      </NumberInput>
     </form>
   </main>
 </template>
+
+<script lang="ts" setup>
+import NumberInput from './NumberInput.vue';
+</script>
