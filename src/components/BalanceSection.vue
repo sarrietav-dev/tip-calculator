a72933557e2f789992df66be7f9c114526ad83b3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-26 02:49:04 -0500

    📱 Increase text size on lg

--- src/components/BalanceSection.vue
+++ src/components/BalanceSection.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex items-center justify-between">
     <div class="flex flex-col">
-      <p class="text-lg text-white font-bold">{{ label }}</p>
+      <p class="text-lg lg:text-base text-white font-bold">{{ label }}</p>
       <p class="text-dark-cyan font-bold">/ person</p>
     </div>
-    <p class="text-3xl text-primary-cyan font-bold">${{ amount }}</p>
+    <p class="text-3xl lg:text-5xl text-primary-cyan font-bold">
+      ${{ amount }}
+    </p>
   </div>
 </template>
 
