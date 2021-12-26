2a053dbc180a6f8e642d4ed442708a51ce1cedf9
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-26 02:57:27 -0500

    🩹 Show 0 when the calculation is infinite

--- src/store/mainStore.ts
+++ src/store/mainStore.ts
@@ -11,18 +11,16 @@ const useStore = defineStore('main', {
   getters: {
     isEmpty: ({ tip, bill, people }) => bill === 0 || tip === 0 || people === 0,
     tipAmount(): number {
-      return this.isEmpty
-        ? 0
-        : Number.parseFloat(
-            ((this.bill * (this.tip / 100)) / this.people).toFixed(2),
-          );
+      const tipAmount = Number.parseFloat(
+        ((this.bill * (this.tip / 100)) / this.people).toFixed(2),
+      );
+      return this.isEmpty || !isFinite(tipAmount) ? 0 : tipAmount;
     },
     totalAmount(): number {
-      return this.isEmpty
-        ? 0
-        : Number.parseFloat(
-            (this.bill / this.people + this.tipAmount).toFixed(1),
-          );
+      const totalAmount = Number.parseFloat(
+        (this.bill / this.people + this.tipAmount).toFixed(1),
+      );
+      return this.isEmpty || !isFinite(totalAmount) ? 0 : totalAmount;
     },
   },
 });
