c2e737a220d8525cd5c31df9bb85f6340791268e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-26 02:53:03 -0500

    📱 Increase paddings on lg

--- src/components/TheBalancePanel.vue
+++ src/components/TheBalancePanel.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="bg-darkest-cyan p-6 pt-10 rounded-xl flex flex-col lg:grow gap-6">
-    <BalanceSection label="Tip Amount" :amount="tipAmount" />
+  <div
+    class="
+      bg-darkest-cyan
+      p-6
+      lg:p-10
+      pt-10
+      lg:pt-14
+      rounded-xl
+      flex flex-col
+      lg:grow
+      gap-6
+    "
+  >
+    <BalanceSection label="Tip Amount" :amount="tipAmount" class="lg:mb-5" />
     <BalanceSection label="Total" :amount="totalAmount" class="lg:mb-auto" />
     <TheResetButton />
   </div>
